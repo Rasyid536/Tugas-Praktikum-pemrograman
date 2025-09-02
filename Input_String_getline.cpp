650239f9c3cec07b97671dbc48cc3e9b5d164cfa
rasyid <68007122+Rasyid536@users.noreply.github.com>
2025-09-02 17:49:29 +0700

    Create Input_String_getline.cpp

---
+++ Input_String_getline.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	string nama;
+
+	cout << "Masukan nama : ";
+	getline(cin, nama);
+	cout << "Nama anda : " << nama;
+	
+	return 0;
+
+}
