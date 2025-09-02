1e72de8e83374850842c19c4c14d0451e36263ba
rasyid <68007122+Rasyid536@users.noreply.github.com>
2025-09-02 12:52:30 +0700

    Create Puts.c

---
+++ Puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+	puts("Halo! Selamat datang di prgram C++ sederhana.");
+	puts("Teknologi Game. ");
+	puts("Politeknik Elektronika Negeri Surabaya. ");
+
+	return 0;
+}
