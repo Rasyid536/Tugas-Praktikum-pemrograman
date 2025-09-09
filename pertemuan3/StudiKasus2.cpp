fb179a27e37275480945de848390c3aab2bfa66a
rasyid <68007122+Rasyid536@users.noreply.github.com>
2025-09-09 20:31:09 +0700

    Create StudiKasus2.cpp

---
+++ pertemuan3/StudiKasus2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main() {
+    // Write C++ code here
+   int skor =0, level = 1;
+   
+   skor += 50;
+   
+   level++;
+   
+   cout << "Skor akhir : "<< skor<<endl;
+   
+   cout << "Sisa skor % 7 "<< skor % 7<<endl;
+   
+   cout<<"Level akhir : "<<level<<endl;
+   
+   return 0;
+}
