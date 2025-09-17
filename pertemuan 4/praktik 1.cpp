e2a46d37a2493ec87b59e0dbbefe11449b61d700
rasyid <68007122+Rasyid536@users.noreply.github.com>
2025-09-17 17:01:55 +0700

    Create praktik 1.cpp

---
+++ pertemuan 4/praktik 1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+int main() {
+    int skor[3];
+    
+    skor[0] = 100;
+    skor[1] = 200;
+    skor[2] = 150;
+    
+    
+    cout << "Skor Player 1 = " << skor [0] << endl;
+    cout << "Skor Player 2 = " << skor [1] << endl;
+    cout << "Skor Player 3 = " << skor [2] << endl;
+    
+    return 0;
+}
