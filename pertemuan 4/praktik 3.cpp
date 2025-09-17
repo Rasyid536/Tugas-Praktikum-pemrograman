5061420b665a93e2b19549450c0dffd62acf152a
rasyid <68007122+Rasyid536@users.noreply.github.com>
2025-09-17 17:03:11 +0700

    Create praktik 3.cpp

---
+++ pertemuan 4/praktik 3.cpp
@@ -0,0 +1,17 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+int main() {
+    int matriks[2][2] = { 
+        {1, 2},
+        {2, 4}
+    };
+    
+    cout << " Isi matriks : \n";
+    
+    cout << matriks[0][0] << " " << matriks[0][1] << endl;
+    cout << matriks[1][0] << " " << matriks[1][1] << endl;
+    return 0;
+}
