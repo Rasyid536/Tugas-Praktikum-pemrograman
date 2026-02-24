cc0ba8a80c96a7a6e895318183e6a8332ee92628
rasyid kawaii <68007122+Rasyid536@users.noreply.github.com>
2026-02-24 13:43:46 +0700

    Create PercobaanTiga.cpp

---
+++ Sem2/PercobaanTiga.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    float d = 54.5f;
+    float* pd;
+
+    cout << "Isi d mula-mula = " << d << endl;
+
+    pd = &d;    
+    *pd += 10;
+
+    cout << "Isi d sekarang = " << d << endl;
+}
